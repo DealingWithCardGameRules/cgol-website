4bd325b21035dc6526a419c79ca1a44a354acd48
Thomas Volden <tv@chargetime.eu>
2022-12-04 22:24:23 +0100

    Moved project relevant extention

---
+++ FluxxConcepts/Queries/PlayLimitAbove.cs
@@ -0,0 +1,16 @@
+﻿using dk.itu.game.msc.cgol.CommonConcepts.Attributes;
+using dk.itu.game.msc.cgol.Distribution;
+
+namespace dk.itu.game.msc.cgol.FluxxConcepts.Queries
+{
+    public class PlayLimitAbove : IQuery<bool>
+    {
+        public int Limit { get; }
+
+        [Concept(Description = "Check if the current play limit is above the specified value.")]
+        public PlayLimitAbove(int limit)
+        {
+            Limit = limit;
+        }
+    }
+}
